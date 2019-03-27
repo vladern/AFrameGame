65326369eacd01de4313f35955507606d0c3464b
vladern <vladys.3234@gmail.com>
2019-03-27 22:50:25 +0100

    refactorización de los mensajes del panel derecho

--- src/app/components/app.module.ts
+++ src/app/components/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -17,6 +17,6 @@ import { GameComponent } from './game/game.component';
   ],
   providers: [],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -8,23 +8,31 @@ import { TextLine } from 'src/app/shared/text-line/text-line.model';
 export class MenuComponent implements OnInit, AfterViewInit, AfterViewChecked {
 
   private _leftPanelLines: TextLine[];
+  private _rightPanelLines: TextLine[];
   constructor() { }
 
   ngOnInit() {
   }
 
-  
+
   ngAfterViewInit() {
-      this._leftPanelLines = [{position: "0 0.8 0", text: "Warnign!!", fontSize: "60px"},
-      
-      {position: "0 0.6 0", text: "This is a alfa version of the game.", fontSize: "40px"},
-      {position: "0 0.4 0", text: "Final version will containt important changes.", fontSize: "40px"},
-      {position: "0 0.2 0", text: "The project is under development and undestand it as such.", fontSize: "40px"},
-      {position: "0 0 0", text: "The project is under development and undestand it as such.", fontSize: "40px"},
-      {position: "0 -0.2 0", text: "Bla bla bla bla bal aklñasdinwn ehreiuan ", fontSize: "40px"},
-      {position: "0 -0.4 0", text: "Ñianei bhehaaie ehqahei na neiei ne hey kn .", fontSize: "40px"},
-      {position: "0 -0.6 0", text: "Ien qiei fhan ei nein añnei .", fontSize: "40px"},
-      ];
+    this._leftPanelLines = [
+      { position: "0 0.8 0", text: "Warnign!!", fontSize: "60px" },
+      { position: "0 0.6 0", text: "This is a alfa version of the game.", fontSize: "40px" },
+      { position: "0 0.4 0", text: "Final version will containt important changes.", fontSize: "40px" },
+      { position: "0 0.2 0", text: "The project is under development and undestand it as such.", fontSize: "40px" },
+      { position: "0 0 0", text: "The project is under development and undestand it as such.", fontSize: "40px" },
+      { position: "0 -0.2 0", text: "Bla bla bla bla bal aklñasdinwn ehreiuan ", fontSize: "40px" },
+      { position: "0 -0.4 0", text: "Ñianei bhehaaie ehqahei na neiei ne hey kn .", fontSize: "40px" },
+      { position: "0 -0.6 0", text: "Ien qiei fhan ei nein añnei .", fontSize: "40px" },
+    ];
+    this._rightPanelLines = [
+      { position: "0 1.05 0", text: "PLAYER STATISTICS", fontSize: "60px" },
+      { position: "-1.06 0.4 0", text: "Levels Played               79", fontSize: "40px" },
+      { position: "-1.06 0.2 0", text: "Levels Cleared              20", fontSize: "40px" },
+      { position: "1.06 0.4 0", text: "Good Cuts                 20005", fontSize: "40px" },
+      { position: "1.06 0.2 0", text: "Bad Cuts                 20", fontSize: "40px" },
+    ];
   }
 
   ngAfterViewChecked() {
@@ -36,6 +44,14 @@ export class MenuComponent implements OnInit, AfterViewInit, AfterViewChecked {
 
   set liftPanelLines(textLines: TextLine[]) {
     this._leftPanelLines = textLines;
+  }
+
+  get rightPanelLines(): TextLine[] {
+    return this._rightPanelLines;
+  }
+
+  set rightPanelLines(textLines: TextLine[]) {
+    this._rightPanelLines = textLines;
   }
 
   onPlay($event) {
