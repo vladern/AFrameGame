59c7ddaea7ba6cade4ff57e55f7573d3aecd19eb
vladern <vladys.3234@gmail.com>
2019-03-27 20:26:57 +0100

    elimino el componente innecesario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CentralScreenComponent } from './central-screen/central-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CentralScreenComponent
   ],
   imports: [
     BrowserModule
