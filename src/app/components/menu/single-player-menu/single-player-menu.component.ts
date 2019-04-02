a5732d2c703a21497c324eb9fa7e58be13a594a6
vladern <vladys.3234@gmail.com>
2019-04-02 16:34:46 +0200

    meto bodones de selecionar canción en la pantalla principal

--- src/app/components/menu/single-player-menu/single-player-menu.component.ts
+++ src/app/components/menu/single-player-menu/single-player-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Song } from 'src/app/shared/song/song.model';
+import { SongLevels } from 'src/app/shared/song/song-levels.enum';
 
 @Component({
   selector: 'a-single-player-menu',
@@ -9,7 +11,35 @@ export class SinglePlayerMenuComponent implements OnInit {
 
   constructor() { }
 
+  private _songs: Song[];
+  private _selectedSong: Song;
+
   ngOnInit() {
-    
+    this._songs = [
+      {name: 'Random song number 1', levels: [SongLevels.easy, SongLevels.normal, SongLevels.hard], timeInSeconds: 90},
+      {name: 'Random song number 2', levels: [SongLevels.easy, SongLevels.normal, SongLevels.hard], timeInSeconds: 90},
+      {name: 'Random song number 3', levels: [SongLevels.easy, SongLevels.normal, SongLevels.hard], timeInSeconds: 90},
+      {name: 'Random song number 4', levels: [SongLevels.easy, SongLevels.normal, SongLevels.hard], timeInSeconds: 90},
+    ];
+  }
+
+  get songs(): Song[] {
+    return this._songs;
+  }
+
+  set songs(songs: Song[]) {
+    this._songs = songs;
+  }
+
+  get songSelected() {
+    return this._selectedSong;
+  }
+
+  set songSelected(songSelected: Song) {
+    this._selectedSong = songSelected;
+  }
+
+  selectTheSong(song: Song) {
+    this.songSelected = song;
   }
 }
