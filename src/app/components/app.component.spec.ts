03f5387d2a80563ee0094b3879b668f8f9531f0b
vladern <vladys.3234@gmail.com>
2019-05-03 14:09:52 +0200

    escribo test que no pasa

--- src/app/components/app.component.spec.ts
+++ src/app/components/app.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -16,5 +16,11 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should exist a camara on center', ()=> {
+    const fixture = TestBed.createComponent(AppComponent);
+    const camara = fixture.debugElement.nativeElement.querySelector('#camara');
+    expect(camara).toBeTruthy();
   });
 });
