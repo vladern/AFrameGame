7a81765466c9e7ff3590fc96636ee5a1f21367cd
vladern <vladys.3234@gmail.com>
2019-05-10 15:37:00 +0200

    refactorización

--- src/app/services/beatsaver.service.ts
+++ src/app/services/beatsaver.service.ts
@@ -8,25 +8,31 @@ export class BeatsaverService {
 
   constructor(private _http: HttpClient) { }
 
+  private readonly _baseURL: string = 'https://beatsaver.com';
+
   public getTOP25SongsJSON() {
     const top25URL: string = 'https://beatsaber-songs.herokuapp.com/top/all/playlist/25';
     return this._http.get(top25URL);
   }
 
   public getSongInformationJSON(key: string) {
-    const songInfoURL: string = 'https://beatsaver.com/api/songs/detail/' + key;
+    const songInfoURL: string = this._baseURL + '/api/songs/detail/' + key;
     return this._http.get(songInfoURL);
   }
 
   public getSongCoverImageURL(key: string) {
-    const songCoverBaseURL: string = 'https://beatsaver.com/storage/songs';
-    const id: string = key.split('-')[0];
+    const songCoverBaseURL: string = this._baseURL + '/storage/songs';
+    const id: string = this._getSongIdFromTheKey(key);
     return songCoverBaseURL + '/' + id + '/' + key + '.png'
   }
 
   public getZipFileToPlayTheSong(key: string) {
-    const songZipFileBaseURL: string = 'https://beatsaver.com/storage/songs/';
-    const id: string = key.split('-')[0];
+    const songZipFileBaseURL: string = this._baseURL + '/storage/songs';
+    const id: string = this._getSongIdFromTheKey(key);
     return this._http.get(songZipFileBaseURL + '/' + id + '/' + key + '.zip');
+  }
+
+  private _getSongIdFromTheKey(key: string): string {
+    return key.split('-')[0];
   }
 }
