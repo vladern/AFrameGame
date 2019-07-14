c17501d50b754e45b5d5967613e2e6a7d76ddd05
vladern <vladys.3234@gmail.com>
2019-07-14 23:22:40 +0200

    The component set box position

--- src/app/components/game/beat/beat.component.spec.ts
+++ src/app/components/game/beat/beat.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BeatComponent } from './beat.component';
+import { Position } from 'src/app/shared/position/position.model';
+import { BeatPosition } from 'src/app/shared/position/beatPosition.model';
+import { HorizontalPositions } from 'src/app/shared/position/horizontalPositions.enum';
+import { VerticalPositions } from 'src/app/shared/position/verticalPositions.enum';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 
 describe('BeatComponent', () => {
   let component: BeatComponent;
@@ -8,8 +14,11 @@ describe('BeatComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BeatComponent ]
+      declarations: [  BeatComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     })
+    .overrideModule(BrowserDynamicTestingModule, 
+      {set: {entryComponents: [BeatComponent]}})
     .compileComponents();
   }));
 
@@ -17,9 +26,28 @@ describe('BeatComponent', () => {
     fixture = TestBed.createComponent(BeatComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    component.ngOnInit();
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('The component recive position', () => {
+    const resEsperado = {horizontalPosition: HorizontalPositions.left, verticalPosition: VerticalPositions.middle};
+    component.beatPosition = resEsperado;
+    expect(component.getBeatInitialPosition().horizontalPosition).toBe(resEsperado.horizontalPosition);
+    expect(component.getBeatInitialPosition().verticalPosition).toBe(resEsperado.verticalPosition);
+  });
+
+  it('The component set box position', () => {
+    const resEsperado = {horizontalPosition: HorizontalPositions.left, verticalPosition: VerticalPositions.middle};
+    component.beatPosition = resEsperado;
+    const boxElement = fixture.debugElement.nativeElement.querySelector('#beatComponent');
+
+    expect(boxElement.object3D.position.x).toBe(-0.75);
+    expect(boxElement.object3D.position.y).toBe(1.2);
+    expect(boxElement.object3D.position.z).toBe(-5);
   });
 });
--- src/app/components/game/beat/beat.component.ts
+++ src/app/components/game/beat/beat.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, AfterViewInit } from '@angular/core';
+import { BeatPosition } from 'src/app/shared/position/beatPosition.model';
+import { HorizontalPositions } from 'src/app/shared/position/horizontalPositions.enum';
+import { VerticalPositions } from 'src/app/shared/position/verticalPositions.enum';
 
 @Component({
   selector: 'a-beat',
   templateUrl: './beat.component.html',
   styleUrls: ['./beat.component.css']
 })
-export class BeatComponent implements OnInit {
+export class BeatComponent implements OnInit, AfterViewInit {
+
+  @Input() beatPosition: BeatPosition;
+  @ViewChild('boxElement') boxElement;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    if ( this.beatPosition !== undefined ) {
+      this.setElementPosition(this.beatPosition);
+    } else {
+      this.setElementPosition({horizontalPosition: HorizontalPositions.left, verticalPosition: VerticalPositions.middle});
+    }
+  }
+
+  getBeatInitialPosition(): BeatPosition {
+    return this.beatPosition;
+  }
+
+  setElementPosition(position: BeatPosition) {
+    switch (position.horizontalPosition) {
+      case HorizontalPositions.left:
+        this.boxElement.nativeElement.object3D.position.x = -0.75;
+        break;
+      case HorizontalPositions.middleLeft:
+        this.boxElement.nativeElement.object3D.position.x = -0.25;
+        break;
+      case HorizontalPositions.middleRight:
+        this.boxElement.nativeElement.object3D.position.x = 0.25;
+        break;
+      case HorizontalPositions.right:
+        this.boxElement.nativeElement.object3D.position.x = 0.75;
+        break;
+      default:
+        break;
+    }
+
+    switch (position.verticalPosition) {
+      case VerticalPositions.bottom:
+        this.boxElement.nativeElement.object3D.position.y = 0.7;
+        break;
+      case VerticalPositions.middle:
+        this.boxElement.nativeElement.object3D.position.y = 1.2;
+        break;
+      case VerticalPositions.top:
+        this.boxElement.nativeElement.object3D.position.y = 1.7;
+        break;
+      default:
+        break;
+    }
+
+    this.boxElement.nativeElement.object3D.position.z = -5;
+
   }
 
 }
