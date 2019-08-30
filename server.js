70ddb7847407f783de6c31ffd46569cd82b095d3
vladern <vladys.3234@gmail.com>
2019-08-30 18:02:50 +0200

    cambioes en server.js para que vaya bien en heroku

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/aframe-game'));
+app.use(express.static(__dirname + '/dist/AFrameGame'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/aframe-game/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/AframeGame/index.html'));
 });
 
 // default Heroku PORT
